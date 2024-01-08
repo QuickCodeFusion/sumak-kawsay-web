e3cd2d0ea7d9f07b84e051bb3467c839400f5b86
juanpo12 <juanjoc2000@hotmail.com>
2024-01-08 18:21:03 -0300

    refactor[reduxProvider]

--- app/reduxProvider.tsx
+++ app/reduxProvider.tsx
@@ -2,10 +2,12 @@
 import { Provider } from 'react-redux'
 import { store } from '@/lib/redux/store'
 
-export function Providers ({ children }: { children: React.ReactNode }): React.JSX.Element {
+const ReduxProvider = ({ children }: { children: React.ReactNode }): React.JSX.Element => {
   return (
         <Provider store={store}>
             {children}
         </Provider>
   )
 }
+
+export default ReduxProvider
