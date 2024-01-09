279e63208b9bbf367ca47c04ed59e59b383ed39e
UrielC2004 <131821174+UFCampos@users.noreply.github.com>
2024-01-09 08:57:38 -0300

    Countdown (#1)
    * feat[components/ui]: added timer component
    
    * feat[components]: added PresaleCountdown component
    
    * feat[main page]: added PresaleCountdown component to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from '@/components/Navbar'
+import PresaleCountdown from '@/components/PresaleCountdown'
 
 const Home = (): React.JSX.Element => {
   return (
     <main>
       <Navbar/>
+      <PresaleCountdown/>
     </main>
   )
 }
---
+++ components/PresaleCountdown.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { Timer } from './ui/timer'
+
+const PresaleCountdown = (): React.JSX.Element => {
+  return (
+        <div className='flex flex-col text-center justify-center items-center my-12'>
+            <h5 className='text-lg uppercase font-semibold text-white'>Pre-sale ends in</h5>
+            <Timer time={30 * 24 * 60 * 60} size={'lg'} />
+        </div>
+  )
+}
+
+export default PresaleCountdown
