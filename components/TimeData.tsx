318416a29a987335fd8dee6eba304cff7561512e
altair201 <wilsondavianpa1998@outlook.com>
2024-01-09 10:12:08 -0500

    feat:[update bar]

--- components/TimeData.tsx
+++ components/TimeData.tsx
@@ -3,10 +3,11 @@ import { Progress } from './ui/progress-bar'
 
 export const TimeData = (): React.JSX.Element => {
   return (
-    <div className='w-1/2 grid place-items-center gap-4 '>
-        <div className='flex justify-between w-full font-bold text-white'><p className='text-x uppercase'>stage 1 : 20% bonus !</p><p className='text-x uppercase'>2000 / 10000</p></div>
-        <Progress value={50} className='h-7 outline-dashed p-1 outline-1 outline-white'/>
-        <ButtonUI className='w-52 uppercase ' variant={'outline'}>
+    <div className='grid place-items-center gap-4 '>
+        <div className='w-3/4 md:w-[1000px] flex justify-between  font-bold text-white'><p className='text-sm uppercase md:text-lg'>stage 1 : 20% bonus !</p><p className='text-sm uppercase md:text-lg'>2000 / 10000</p>
+        </div>
+        <Progress value={30} className='w-3/4 md:w-[1000px] h-7 md:h-10 outline-dashed p-1 outline-1 outline-white'/>
+        <ButtonUI className='w-52 uppercase rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 border-none text-white' variant={'outline'}>
             buy now
         </ButtonUI>
     </div>
