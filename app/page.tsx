cf5929aa7ba828e707b37edbc24e6deb4de00a2f
Uriel Campos <urielcampos448@gmail.com>
2024-01-09 12:45:37 -0300

    feat[home page]: added placeholder greeting text

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,14 @@ const Home = (): React.JSX.Element => {
     <main>
       <Navbar/>
       <PresaleCountdown/>
+      <section>
+        <div className='flex flex-col text-center text-white justify-center gap-10 md:gap-16 items-center my-12'>
+          <h1 className='text-4xl md:text-6xl font-bold'>
+            Unified Decentralized Platform
+          </h1>
+          <p className='text-md md:text-lg font-semibold'>Buy tokens now and reap the benefits of the blockchain revolution!</p>
+        </div>
+      </section>
       <TimeData/>
     </main>
   )
