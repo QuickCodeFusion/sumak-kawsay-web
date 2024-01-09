a04460a2e9d486f236edd6c51c511d8abff6a0ef
Uriel Campos <urielcampos448@gmail.com>
2024-01-08 21:23:04 -0300

    feat[app]: Added providers to root layout + and installed rainbow kit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,7 +23,11 @@ const RootLayout = ({
           'min-h-screen',
           inter.className
         )
-      }>{children}</body>
+      }>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
