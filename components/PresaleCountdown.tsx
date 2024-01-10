65c015420dacde765e7bfa8889316b015c51fb06
juanpo12 <juanjoc2000@hotmail.com>
2024-01-10 16:35:58 -0300

    fix[home]:Adjusted main height

--- components/PresaleCountdown.tsx
+++ components/PresaleCountdown.tsx
@@ -4,7 +4,7 @@ import { Timer } from './ui/timer'
 
 const PresaleCountdown = (): React.JSX.Element => {
   return (
-        <div className='flex flex-col text-center justify-center items-center my-12'>
+        <div className='flex flex-col  text-center justify-center items-center my-10'>
             <h5 className='text-lg uppercase font-semibold text-white'>Pre-sale ends in</h5>
             <Timer time={30 * 24 * 60 * 60} size={'lg'} />
         </div>
