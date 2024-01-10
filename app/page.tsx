cf11d762179aaa925feae8f6ec47ab5676937f35
Juan José Diaz <123817829+juanpo12@users.noreply.github.com>
2024-01-10 16:37:16 -0300

    Navbar (#10)
    * feat[assets]: Added whitepaper pdf
    
    * feat[pdf]:Added new windows PDF
    
    * feat[TS]: Added type
    
    * feat[connectButton]:Added connect Button and darkthem
    
    * feat[button]:Added buttontransaction
    
    * feat[transaction]:Created button transaction
    
    * fix[connectButton]:changed name Connect
    
    * fix[home]:Adjusted main height

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { ButtonModal } from '@/components/ButtonModal'
 
 const Home = (): React.JSX.Element => {
   return (
-    <main className='h-[77vh] md:h-[81vh] grid place-items-center'>
+    <main className='h-5/6 flex gap-4 flex-col justify-center'>
       <PresaleCountdown/>
       <section>
         <div className='flex flex-col text-center text-white justify-center gap-10 md:gap-16 items-center my-12'>
