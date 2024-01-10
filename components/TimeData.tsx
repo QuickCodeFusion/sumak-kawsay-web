47d9df72d523f68fcc95a300b21ffa659df2921a
UrielC2004 <131821174+UFCampos@users.noreply.github.com>
2024-01-10 16:01:35 -0300

    Production build #2 (#9)
    * feat[eslint]: Added new rule
    
    * refactor[navigationmenu]:format
    
    * feat[Navbar]: added buttons
    
    * feat[reduxtlk]:Instaled
    
    * feat[reduxtlk]:Added configuration provider
    
    * refactor[reduxProvider]
    
    * feat: Added rainbowKit provider
    
    * feat: Added main providers component
    
    * feat[app]: Added providers to root layout + and installed rainbow kit
    
    * feat[public/] added background image
    
    * feat:[background-image]
    
    * Countdown (#1)
    
    * feat[components/ui]: added timer component
    
    * feat[components]: added PresaleCountdown component
    
    * feat[main page]: added PresaleCountdown component to home
    
    * Altair/progrest bar (#2)
    
    * feat:[Progress bar]
    
    * feat:[update bar]
    
    * Navbar (#3)
    
    * feat[assets]: Added whitepaper pdf
    
    * feat[pdf]:Added new windows PDF
    
    * feat[TS]: Added type
    
    * feat[connectButton]:Added connect Button and darkthem
    
    * feat[button]:Added buttontransaction
    
    * feat[transaction]:Created button transaction
    
    * feat[home page]: added placeholder greeting text
    
    * Juan footer (#4)
    
    * feat[assets]: Added whitepaper pdf
    
    * feat[pdf]:Added new windows PDF
    
    * feat[TS]: Added type
    
    * feat[connectButton]:Added connect Button and darkthem
    
    * feat[button]:Added buttontransaction
    
    * feat[transaction]:Created button transaction
    
    * fix[connectButton]:changed name Connect
    
    * refactor[Navbar]:Moved Navbar
    
    * feat: Added Footer
    
    * fix:Fixed screen in smartphone
    
    * Altair/progrest bar (#5)
    
    * feat:[Progress bar]
    
    * feat:[update bar]
    
    * feat:[modal]
    
    * fix:[center page]
    
    * feat[TimeData] progressbar now has dyn values (#6)
    
    * Altair/progrest bar (#7)
    
    * feat:[Progress bar]
    
    * feat:[update bar]
    
    * feat:[modal]
    
    * fix:[center page]
    
    * feat:[edit modal]
    
    * fix[components/ui]:Missing eslint rules
    
    * feat[timedata]:Deleted button "buy now"
    
    * fix[TimeData] percentage now updates once per page load
    
    ---------
    
    Co-authored-by: juanpo12 <juanjoc2000@hotmail.com>
    Co-authored-by: altair201 <wilsondavianpa1998@outlook.com>
    Co-authored-by: Juan José Diaz <123817829+juanpo12@users.noreply.github.com>

--- components/TimeData.tsx
+++ components/TimeData.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { ButtonUI } from './ui/button'
 import { Progress } from './ui/progress-bar'
 
 interface props {
@@ -12,7 +11,7 @@ export const TimeData: React.FC<props> = ({ stage, progress, goal }): React.JSX.
   const [percentage, setPercentage] = useState(0)
   useEffect(() => {
     setPercentage((progress / goal) * 100)
-  }, [stage, progress, goal])
+  }, [])
   return (
     <div className='grid place-items-center gap-4 '>
         <div className='w-3/4 md:w-[1000px] flex justify-between  font-bold text-white'>
@@ -24,9 +23,6 @@ export const TimeData: React.FC<props> = ({ stage, progress, goal }): React.JSX.
           </p>
         </div>
         <Progress value={percentage} className='w-3/4 md:w-[1000px] h-7 md:h-10 outline-dashed outline-1 outline-white'/>
-        <ButtonUI className='w-52 uppercase rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 border-none text-white' variant={'outline'}>
-            buy now
-        </ButtonUI>
     </div>
   )
 }
