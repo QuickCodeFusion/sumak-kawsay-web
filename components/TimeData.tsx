c417e1c194b107288795670a722e5675c45db7b8
Uriel Campos <urielcampos448@gmail.com>
2024-01-10 08:57:55 -0300

    feat[TimeData] progressbar now has dyn values

--- components/TimeData.tsx
+++ components/TimeData.tsx
@@ -1,12 +1,29 @@
+'use client'
+import { useEffect, useState } from 'react'
 import { ButtonUI } from './ui/button'
 import { Progress } from './ui/progress-bar'
 
-export const TimeData = (): React.JSX.Element => {
+interface props {
+  stage: number
+  progress: number
+  goal: number
+}
+export const TimeData: React.FC<props> = ({ stage, progress, goal }): React.JSX.Element => {
+  const [percentage, setPercentage] = useState(0)
+  useEffect(() => {
+    setPercentage((progress / goal) * 100)
+  }, [stage, progress, goal])
   return (
     <div className='grid place-items-center gap-4 '>
-        <div className='w-3/4 md:w-[1000px] flex justify-between  font-bold text-white'><p className='text-sm uppercase md:text-lg'>stage 1 : 20% bonus !</p><p className='text-sm uppercase md:text-lg'>2000 / 10000</p>
+        <div className='w-3/4 md:w-[1000px] flex justify-between  font-bold text-white'>
+          <p className='text-sm uppercase md:text-lg'>
+            stage {stage} : {percentage}% bonus !
+          </p>
+          <p className='text-sm uppercase md:text-lg'>
+            {progress} / {goal} ETH
+          </p>
         </div>
-        <Progress value={30} className='w-3/4 md:w-[1000px] h-7 md:h-10 outline-dashed p-1 outline-1 outline-white'/>
+        <Progress value={percentage} className='w-3/4 md:w-[1000px] h-7 md:h-10 outline-dashed outline-1 outline-white'/>
         <ButtonUI className='w-52 uppercase rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 border-none text-white' variant={'outline'}>
             buy now
         </ButtonUI>
