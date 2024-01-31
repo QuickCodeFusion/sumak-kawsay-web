3cc7316f19a24de5c17be1fb1697f6111977fca0
Uriel Campos <urielcampos448@gmail.com>
2024-01-31 12:23:43 -0300

    fixed footer linter errors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,43 +1,7 @@
-'use client'
-
-import Image from 'next/image'
-import Link from 'next/link'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger
-} from '@/components/ui/tooltip'
-
 const Footer = (): React.JSX.Element => {
-  const networks = [
-    {
-      id: 1,
-      title: 'Facebook',
-      url: 'https://www.facebook.com/TransformationalFestivals',
-      image: '/facebook.png'
-    },
-    {
-      id: 2,
-      title: 'Instagram',
-      url: 'https://www.instagram.com/transformationalfestivals/?utm_source=qr&igsh=d29iY3Rld3kzc25u',
-      image: '/instagram.png'
-    },
-    {
-      id: 3,
-      title: 'Binance',
-      url: 'https://www.binance.com/es/nft/item/82689062?unverified=1&fbclid=IwAR2yoQgi_fva_e3uw4bT5_UgJxgoq1sbv_fDuDXgO1B2weDMpBE-RXvwrfo',
-      image: '/binance.png'
-    }
-  ]
-
-  const readPDF = (): void => {
-    window.open('/whitepaper.pdf', '_blank')
-  }
-
   return (
     <footer className="flex mt-5 p-5 justify-center gap-6">
-        
+
     </footer>
   )
 }
