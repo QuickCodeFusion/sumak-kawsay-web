9a2d84e1a74ef150735d47dc038c0fb9441b0130
juanpo12 <juanjoc2000@hotmail.com>
2024-02-19 16:53:30 -0300

    feat[view]:Created two views

---
+++ components/FinalView.tsx
@@ -0,0 +1,16 @@
+import ClaimSteps from './ClaimSteps'
+
+const FinalView = (): React.JSX.Element => {
+  return (
+        <main className='flex flex-col md:flex-row gap-4 pt-8 justify-center w-screen'>
+            {/* <section className='w-2/4'>
+
+            </section> */}
+            <section className='p-4 md:p-0 '>
+                  <ClaimSteps/>
+            </section>
+        </main>
+  )
+}
+
+export default FinalView
