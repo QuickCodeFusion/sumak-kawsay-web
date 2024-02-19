1565b2e20e43dd28fdccc7635291a0014a066620
juanpo12 <juanjoc2000@hotmail.com>
2024-02-19 16:51:30 -0300

    feat[page]: Added a finalView and preView for date

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,20 @@
-import BuyInfo from '@/components/BuyInfo'
-import PresaleCountdown from '@/components/PresaleCountdown'
-import { ButtonUI } from '@/components/ui/button'
-import Link from 'next/link'
-import InfoPhase from '@/utils/functionsContract/InfoPhase'
-import Claim from '@/components/Claim'
+import PreView from '@/components/PreView'
+import FinalView from '@/components/FinalView'
+
+const currentDate = new Date()
+const stopRenderingDate = new Date('2024-04-01')
+
+const shouldRenderComponents = currentDate > stopRenderingDate
 
 const Home = (): React.JSX.Element => {
   return (
-    <main className='flex flex-col md:flex-row gap-4 pt-8 justify-center w-screen'>
-      <section>
-        <div className='flex flex-col text-center text-white justify-center gap-10 md:gap-16 items-center my-12'>
-          <h1 className='text-4xl md:text-6xl font-bold'>
-            Unified Decentralized Platform
-          </h1>
-          <p className='text-md md:text-lg font-semibold'>Buy tokens now and reap the benefits of the blockchain revolution!</p>
-          <ButtonUI asChild className='rounded-full capitalize'>
-            <Link href='/whitepaper.pdf' target='_blank'>
-              Read the whitepaper
-            </Link>
-          </ButtonUI>
-        </div>
-      </section>
-      <section className='backdrop-blur bg-black/50 border rounded-3xl p-4 flex flex-col w-full md:w-1/2 mx-0 md:mx-12'>
-        <PresaleCountdown/>
-        <InfoPhase/>
-        <BuyInfo/>
-      </section>
-      {/* <section className='backdrop-blur bg-white border rounded-3xl overflow-hidden flex flex-col min-w-[400px] md:w-1/2 mx-0 md:mx-12'>
-        <Claim/>
-      </section> */}
-    </main>
+      <>
+        {
+          shouldRenderComponents
+            ? <PreView/>
+            : <FinalView/>
+        }
+      </>
   )
 }
 
