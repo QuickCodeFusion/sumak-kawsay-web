a092716507f28e3e83c303b1a310ec34031f1ae8
juanpo12 <juanjoc2000@hotmail.com>
2024-02-26 07:46:23 -0300

    refactor[WhitePaper]: Change for a button

--- components/PreView.tsx
+++ components/PreView.tsx
@@ -1,23 +1,10 @@
 'use client'
-import { ButtonUI } from './ui/button'
 import PresaleCountdown from './PresaleCountdown'
 import InfoPhase from './functionsContract/InfoPhase'
 import BuyInfo from './BuyInfo'
-import Modal from './Modal'
-import WhitePaper from './WhitePaper'
-import { useState } from 'react'
+import ButtonWhitePaper from './ButtonWhiterPaper'
 
 const PreView = (): React.JSX.Element => {
-  const [open, setOpen] = useState(false)
-
-  const handleButtonClick = (): void => {
-    if (window.screen.width < 768) {
-      window.open('/whitepaper.pdf', '_blank')
-    } else {
-      setOpen(true)
-    }
-  }
-
   return (
         <main className='flex flex-col md:flex-row gap-4 pt-8 justify-center w-screen'>
         <section>
@@ -26,12 +13,7 @@ const PreView = (): React.JSX.Element => {
               Unified Decentralized Platform
             </h1>
             <p className='text-md md:text-lg font-semibold'>Buy tokens now and reap the benefits of the blockchain revolution!</p>
-            <ButtonUI onClick={handleButtonClick} className='rounded-full'>
-              Read the whitepaper
-            </ButtonUI>
-            <Modal open={open} setOpen={setOpen} className='w-screen p-2 md:p-0'>
-              <WhitePaper/>
-            </Modal>
+            <ButtonWhitePaper />
           </div>
         </section>
           <section className='backdrop-blur bg-black/50 border rounded-3xl p-4 flex flex-col w-full md:w-1/2 mx-0 md:mx-12'>
