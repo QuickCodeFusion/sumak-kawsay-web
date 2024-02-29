79a1a007571507e886b959829017e38ee7264308
juanpo12 <juanjoc2000@hotmail.com>
2024-02-29 11:46:33 -0300

    feat[CardWork]: Added component to members work

---
+++ components/CardTeamWorkList.tsx
@@ -0,0 +1,39 @@
+import CardTeamWork from './CardTeamWork'
+
+const CardTeamWorkList = (): React.JSX.Element => {
+  const user = [
+    {
+      name: 'Diego Pepe',
+      role: 'CMO',
+      img: '/jefe.jpeg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      name: 'Diego Pepe',
+      role: 'CMO',
+      img: '/jefe.jpeg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      name: 'Diego Pepe',
+      role: 'CMO',
+      img: '/jefe.jpeg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  ]
+  return (
+        <div>
+          <h1 className='text-3xl font-bold text-amber-500'>Team Work</h1>
+          <div className='grid grid-cols-1 md:grid-cols-3 p-4 md:p-0 gap-20 mt-4'>
+            {
+              user.map((user) => (
+                  <CardTeamWork user={user} key={user.name}/>
+              ))
+            }
+          </div>
+        </div>
+
+  )
+}
+
+export default CardTeamWorkList
