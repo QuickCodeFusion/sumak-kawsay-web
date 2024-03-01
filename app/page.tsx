bd70ff85353e68c22c9fce1ae11a4dc9db76479a
altair201 <wilsondavianpa1998@outlook.com>
2024-03-01 08:52:27 -0500

    fix:[save]

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import { Card } from '@/components/ui/card'
 import { InfoItemsList } from '@/components/InfoItemsList'
 import ItemCarousel from '@/components/ItemCarousel'
 import { InfoCard } from '@/components/InfonCard'
+import { Passport } from '@/components/Passport'
 
 // import PresaleCountdown from '@/components/PresaleCountdown'
 // import InfoPhase from '@/components/functionsContract/InfoPhase'
@@ -50,6 +51,7 @@ const Home = (): React.JSX.Element => {
             <Card className='border-none z-10 w-10/12 bg-amber-700/50  backdrop-filter backdrop-blur-lg'>
                   <InfoContent></InfoContent>
             </Card>
+            <Passport/>
             <Footer/>
         </main>
   )
