75b16dafca81d563fec68eb6b35f4f8e447b8ab0
Uriel Campos <urielcampos448@gmail.com>
2024-03-06 08:48:30 -0300

    fixed contract types

--- components/functionsContract/InfoPhase.tsx
+++ components/functionsContract/InfoPhase.tsx
@@ -5,6 +5,13 @@ import { TimeData } from '@/components/TimeData'
 import { setCurrentPrice } from '@/lib/redux/feature/currentPriceSlice'
 import { useDispatch } from '@/lib/redux/hooks'
 
+export interface ContractData {
+  phase: string
+  total: number
+  sold: number
+  price: number
+}
+export type Data = [ContractData]
 const InfoPhase = (): JSX.Element => {
   const dispatch = useDispatch()
   const { data } = useContractRead({
@@ -12,11 +19,12 @@ const InfoPhase = (): JSX.Element => {
     abi: AbyContractAddress,
     functionName: 'currentPhase'
   })
+  const [newData] = data as Data
   console.log(data)
-  const phase = String(data !== undefined && (data as unknown[])[0])
-  const price = Number(data !== undefined && (data as unknown[])[2]) / 10 ** 18
-  const total = Number(data !== undefined && (data as unknown[])[1])
-  const sold = Number(data !== undefined && (data as unknown[])[3])
+  const phase = String(data !== undefined && (newData.total))
+  const price = Number(data !== undefined && (newData.phase)) / 10 ** 18
+  const total = Number(data !== undefined && (newData.price))
+  const sold = Number(data !== undefined && (data as unknown[])[1])
   dispatch(setCurrentPrice(price))
   return <TimeData stage={phase ?? ''} progress={sold ?? 0} goal={total ?? 0} value={price ?? 0} />
 }
