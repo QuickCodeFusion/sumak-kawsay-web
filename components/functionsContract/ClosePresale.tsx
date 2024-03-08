a30ee97d03ae453a06dfa60bccaca38bf1acbede
juanpo12 <juanjoc2000@hotmail.com>
2024-03-08 20:33:56 -0300

    refactor: Changed old function for new hook

--- components/functionsContract/ClosePresale.tsx
+++ components/functionsContract/ClosePresale.tsx
@@ -1,19 +1,18 @@
 'use client'
-import { useContractRead, useAccount, useContractWrite, useWaitForTransaction } from 'wagmi'
+import { useAccount, useContractWrite, useWaitForTransaction } from 'wagmi'
 import { AbyContractAddress, contract } from '@/utils/AbyContrat'
 import { ButtonUI } from '../ui/button'
 import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
 import { CloseIcon } from '../Icons/CloseIcon'
+import { useOwner } from '@/utils/useOwner'
 
 const ClosePresale = (): JSX.Element => {
   const [Shown, setShown] = useState(false)
   const { address } = useAccount()
-  const { data: owner } = useContractRead({
-    address: contract,
-    abi: AbyContractAddress,
-    functionName: 'owner'
-  })
+  const owner = useOwner()
+  console.log(owner)
+
   const { writeAsync, isLoading, data } = useContractWrite({
     address: contract,
     abi: AbyContractAddress,
--- components/functionsContract/ReclaimUsdt.tsx
+++ components/functionsContract/ReclaimUsdt.tsx
@@ -1,19 +1,16 @@
 'use client'
-import { useContractRead, useAccount, useContractWrite, useWaitForTransaction } from 'wagmi'
+import { useAccount, useContractWrite, useWaitForTransaction } from 'wagmi'
 import { AbyContractAddress, contract } from '@/utils/AbyContrat'
 import { ButtonUI } from '../ui/button'
 import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
 import { DollarIcon } from '../Icons/DollarIcon'
+import { useOwner } from '@/utils/useOwner'
 
 const ReclaimUsdt = (): JSX.Element => {
   const [Shown, setShown] = useState(false)
   const { address } = useAccount()
-  const { data: owner } = useContractRead({
-    address: contract,
-    abi: AbyContractAddress,
-    functionName: 'owner'
-  })
+  const owner = useOwner()
   const { writeAsync, isLoading, data } = useContractWrite({
     address: contract,
     abi: AbyContractAddress,
