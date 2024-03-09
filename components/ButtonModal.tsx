f7cc947b674c331486cbbace55fa6e40694411f1
altair201 <wilsondavianpa1998@outlook.com>
2024-03-09 15:01:16 -0500

    fix:[]

--- components/ButtonModal.tsx
+++ components/ButtonModal.tsx
@@ -64,11 +64,11 @@ export const ButtonModal = (): React.JSX.Element => {
         </DialogHeader>
         <div className="grid gap-4 py-4 z-10">
           <div className="grid grid-cols-5 gap-1 ">
-            <Label htmlFor="amount" className='uppercase col-span-5 '>amount</Label>
+            <Label htmlFor="amount" className='uppercase col-span-5 '>USDT Pay</Label>
             <Input id="amount" type='number' onChange={handlesend} value={value.send} className="col-span-3 appearance-none rounded-e-none" /><SelectCoin/>
           </div>
           <div className="grid gap-1 z-10">
-            <Label htmlFor="get-amount" className='uppercase'>amount</Label>
+            <Label htmlFor="get-amount" className='uppercase'>UNITY To receive </Label>
             <Input id="get-amount" type='number' value={value.amount !== 0 ? value.amount : ''} onChange={handleamount} className="col-span-3 appearance-none         " />
           </div>
           <div className='grid divide-y divide-dashed gap-4'>
