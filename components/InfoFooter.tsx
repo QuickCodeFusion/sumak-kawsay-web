c76605b18839269bb475b42c27adfa5ee588dc21
Uriel Campos <urielcampos448@gmail.com>
2024-03-09 23:09:18 -0300

    adjusted footer

--- components/InfoFooter.tsx
+++ components/InfoFooter.tsx
@@ -5,13 +5,12 @@ import IconSocial from './IconSocial'
 const InfoFooter = (): JSX.Element => {
   const year = new Date().getFullYear()
   return (
-    <div className='container flex items-center justify-around flex-col lg:grid grid-cols-3 gap-4 p-4 text-white'>
+    <div className='container flex items-center justify-around flex-col md:grid grid-cols-3 place-items-center gap-4 p-4 text-white'>
         <span>
-          <div className='flex gap-4 items-center ring-offset-2'>
+          <div className='flex gap-4 items-center'>
             <Image className='rounded-full' src={Logo} alt='logo' width={50} height={50}/>
             <p className='font-bold text-2xl'>Sumak Kawsay</p>
           </div>
-          <p className='mt-4 self-end'>The Sumak Kawsay Ecosystem blends asset tokenization, artificial intelligence, decentralized governance, and privacy, fostering a legal and political framework for global articulation, collective prosperity, and sovereignty. This approach ensures ethical financial systems and unified actions for the well-being of Earth.</p>
         </span>
         <IconSocial/>
         <p>© {year} Sumak Kawsay | All rights reserved</p>
