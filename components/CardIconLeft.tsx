fc98da88b9bdc2f3735b7325e4bb2d1ea958a358
altair201 <wilsondavianpa1998@outlook.com>
2024-03-11 17:03:55 -0500

    fix:[]

--- components/CardIconLeft.tsx
+++ components/CardIconLeft.tsx
@@ -5,7 +5,7 @@ export const CardIconLeft = ({ item }: { item: { title: string, description: str
   return (
         <Card className='w-80 bg-background/50 rounded-3xl border shadow-sm flex flex-col '>
           <CardHeader className='flex flex-row gap-5 items-center'>
-            <Image className='ring-2 my-4 ring-offset-2 rounded-full' src={item.icon} alt={item.title} width={50} height={50}></Image>
+            <Image className=' rounded-full' src={item.icon} alt={item.title} width={70} height={70}></Image>
             <CardTitle className='font-normal text-lg'>{item.title}</CardTitle>
           </CardHeader>
           <CardContent>{item.description}</CardContent>
