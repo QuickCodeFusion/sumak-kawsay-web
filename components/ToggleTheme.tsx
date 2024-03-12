399d44cd505e322cd90f0742492ee99704918307
Uriel Campos <urielcampos448@gmail.com>
2024-03-11 22:12:58 -0300

    fixed toggle button width

--- components/ToggleTheme.tsx
+++ components/ToggleTheme.tsx
@@ -18,7 +18,7 @@ export const ModeToggle = (): React.JSX.Element => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <ButtonUI variant="outline" size="icon">
+        <ButtonUI variant="outline" size="icon" className='min-w-[2.5rem] bg-background/65 border-none'>
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
