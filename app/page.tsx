a45cc0c98c2c21e0803e51e428c2955ce8a156dd
juanpo12 <juanjoc2000@hotmail.com>
2024-03-13 12:32:02 -0300

    FEAT

--- app/page.tsx
+++ app/page.tsx
@@ -37,9 +37,9 @@ const shouldRenderProgressBar = currentDate < stopRenderingDate
 
 const Home = (): React.JSX.Element => {
   return (
-        <main className='flex flex-col gap-20 pt-8 mt-10 px-8 backdrop-blur-md justify-center items-center w-screen'>
+        <main className='flex flex-col gap-20 pt-8 mt-10 px-8 backdrop--md justify-center items-center w-screen'>
             <section className='p-2 my-10 z-10 w-full '>
-                  <div className='flex xl:grid grid-cols-3 flex-col place-items-start items-center gap-4 w-full '>
+                  <div className='flex xl:grid grid-cols-3 flex-col place-items-start items-center gap-4 w-full'>
                       <InfoPresale/>
                       {!shouldRenderProgressBar && <ClaimSteps/>}
                       {shouldRenderProgressBar && <section className='bg-background/50 border rounded-3xl py-4 flex flex-col mx-0 w-full md:w-[500px] xl:w-11/12'>
