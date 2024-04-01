f1462b4d90562e760c690e77f2be51b01d15c663
juanpo12 <juanjoc2000@hotmail.com>
2024-04-01 13:58:16 -0300

    feat[dividens]: translated text

--- components/Dividends.tsx
+++ components/Dividends.tsx
@@ -1,4 +1,52 @@
 import Image from 'next/image'
+
+const text = {
+  1: {
+    en: 'Dividends and Staking: A Path to Universal Income',
+    es: 'Dividendos y Staking: Un Camino hacia el Ingreso Universal',
+    pt: 'Dividendos e Staking: Um Caminho para a Renda Universal'
+  },
+  2: {
+    en: 'Experience the joy of giving and receiving. Engage with our ecosystem, stake your tokens, and earn dividends.',
+    es: 'Experimenta la alegría de dar y recibir. Participa en nuestro ecosistema, apuesta tus tokens y gana dividendos.',
+    pt: 'Experimente a alegria de dar e receber. Engaje-se com nosso ecossistema, aplique seus tokens e ganhe dividendos.'
+  },
+  3: {
+    en: 'Empowerment Tiers of Engagement',
+    es: 'Niveles de Empoderamiento de Participación',
+    pt: 'Níveis de Empoderamento de Engajamento'
+  },
+  4: {
+    en: 'Unity Tier:',
+    es: 'Nivel de Unidad:',
+    pt: 'Nível de Unidade:'
+  },
+  5: {
+    en: 'This is your starting point within the Sumak Kawsay collective, offering an introduction to our values and initial dividends. It represents the foundational step towards inclusivity and shared purpose, aligning with our ethos and community spirit.',
+    es: 'Este es tu punto de partida dentro del colectivo Sumak Kawsay, ofreciendo una introducción a nuestros valores y dividendos iniciales. Representa el paso fundamental hacia la inclusividad y el propósito compartido, alineándose con nuestro ethos y espíritu comunitario.',
+    pt: 'Este é o seu ponto de partida dentro do coletivo Sumak Kawsay, oferecendo uma introdução aos nossos valores e dividendos iniciais. Representa o passo fundamental em direção à inclusividade e ao propósito compartilhado, alinhando-se com nosso ethos e espírito comunitário.'
+  },
+  6: {
+    en: 'Growth Tier:',
+    es: 'Nivel de Crecimiento:',
+    pt: 'Nível de Crescimento:'
+  },
+  7: {
+    en: 'For members increasing their involvement, this tier provides greater dividends and signifies a deeper commitment to our collective future. It represents a middle ground for those actively contributing to and engaging with our ecosystem\'s expansion.',
+    es: 'Para los miembros que aumentan su participación, este nivel proporciona mayores dividendos y significa un compromiso más profundo con nuestro futuro colectivo. Representa un terreno intermedio para aquellos que contribuyen activamente y se involucran con la expansión de nuestro ecosistema.',
+    pt: 'Para membros que aumentam seu envolvimento, este nível fornece maiores dividendos e significa um compromisso mais profundo com o nosso futuro coletivo. Representa um meio termo para aqueles que contribuem ativamente e se envolvem com a expansão do nosso ecossistema.'
+  },
+  8: {
+    en: 'Inclusion Tier:',
+    es: 'Nivel Herencia:',
+    pt: 'Nível Herança:'
+  },
+  9: {
+    en: 'The highest level, rewarding our most committed members, symbolizing a significant contribution to our vision and goals. This tier is for those aiming to create a meaningful, lasting impact, ensuring a legacy of prosperity, sustainability, and community well-being.',
+    es: 'El nivel más alto, recompensando a nuestros miembros más comprometidos, simbolizando una contribución significativa a nuestra visión y objetivos. Este nivel es para aquellos que buscan crear un impacto significativo y duradero, asegurando un legado de prosperidad, sostenibilidad y bienestar comunitario.',
+    pt: 'O nível mais alto, recompensando nossos membros mais comprometidos, simbolizando uma contribuição significativa para a nossa visão e objetivos. Este nível é para aqueles que visam criar um impacto significativo e duradouro, garantindo um legado de prosperidade, sustentabilidade e bem-estar comunitário.'
+  }
+}
 export const Dividends = (): JSX.Element => {
   return (
         <div className='my-20'>
