028a7f70a7253f9dbf9e9066390ec91a8fa70869
juanpo12 <juanjoc2000@hotmail.com>
2024-04-05 09:55:29 -0300

    feat flag

--- components/LanguageButton.tsx
+++ components/LanguageButton.tsx
@@ -10,18 +10,18 @@ const LanguageButton = (): React.JSX.Element => {
     <DropdownMenu>
         <DropdownMenuTrigger asChild>
         <ButtonUI variant="outline" size="icon" className='min-w-[2.5rem] bg-background/65 border-none'>
-            {language === 'es' && <Image src='/argentina.png' alt='argentina' width={24} height={24}></Image>}
-            {language === 'en' && <Image src='/bandera.png' alt='argentina' width={24} height={24}></Image>}
+            {language === 'es' && <Image src='/mxflag.png' alt='español' width={24} height={24}></Image>}
+            {language === 'en' && <Image src='/bandera.png' alt='ingles' width={24} height={24}></Image>}
             {language === 'pt' && <Image src='/brasil.png' alt='brasil' width={24} height={24}></Image>}
         </ButtonUI>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
             <DropdownMenuItem onClick={() => { setLanguage('es') }} className='flex gap-2 p-2'>
-                <Image src='/argentina.png' alt='argentina' width={24} height={24}></Image>
+                <Image src='/mxflag.png' alt='español' width={24} height={24}></Image>
                     Español
             </DropdownMenuItem>
             <DropdownMenuItem onClick={() => { setLanguage('en') }} className='flex gap-2 p-2'>
-                <Image src='/bandera.png' alt='argentina' width={24} height={24}></Image>
+                <Image src='/bandera.png' alt='ingles' width={24} height={24}></Image>
                     English
             </DropdownMenuItem>
             <DropdownMenuItem onClick={() => {
