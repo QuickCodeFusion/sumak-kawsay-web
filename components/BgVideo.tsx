fb16fa15e536c948fcb783278739c0feae5e5732
Juan José Diaz <123817829+juanpo12@users.noreply.github.com>
2024-04-14 22:12:54 -0300

    fix image mobile (#43) (#44)

--- components/BgVideo.tsx
+++ components/BgVideo.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 const BgVideo = (): JSX.Element => {
   return (
     <div className="absolute inset-0 h-[180vh] md:h-full w-full overflow-hidden bg-cover bg-no-repeat bg-center -z-10">
@@ -11,6 +13,7 @@ const BgVideo = (): JSX.Element => {
             <source src="/bgVideo.mp4" />
             Tu navegador no admite la reproducción de videos.
         </video>
+          <Image src="/bgm3.svg" className='h-full w-full object-cover md:hidden' alt="bg" width={400} height={400} />
     </div>
   )
 }
