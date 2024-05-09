54038721020055c4b11869698d55dac1a31274c1
Juan José Diaz <123817829+juanpo12@users.noreply.github.com>
2024-05-09 16:38:23 -0300

    added to main (#47)
    * fix image mobile (#43)
    
    * change bg
    
    * change text tokenomic
    
    * fix[navbar]:Change title roadmap
    
    * feat[page]:Added new video

---
+++ components/Video.tsx
@@ -0,0 +1,9 @@
+const Video = (): JSX.Element => {
+  return (
+        <div className="m-20 w-full md:w-4/5">
+            <iframe className="aspect-video w-full rounded border-2 border-foreground" src="https://www.youtube.com/embed/9LLjHXUlxqg?si=MJLdDvGRl33g9vDa" title="YouTube video player" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </div>
+  )
+}
+
+export default Video
