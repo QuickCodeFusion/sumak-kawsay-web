674bedfe71eef18ca4b45f0e7097e28bdf6daa20
UrielC2004 <urielcampos448@gmail.com>
2024-05-09 19:14:10 -0300

    Update Video.tsx

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,7 +1,7 @@
 const Video = (): JSX.Element => {
   return (
         <div className="m-20 w-full md:w-4/5">
-            <iframe className="aspect-video w-full rounded border-2 border-foreground" src="https://www.youtube.com/embed/9LLjHXUlxqg?si=MJLdDvGRl33g9vDa" title="YouTube video player" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <iframe className="aspect-video w-full rounded border-none" src="https://www.youtube.com/embed/9LLjHXUlxqg?si=MJLdDvGRl33g9vDa" title="YouTube video player" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
   )
 }
