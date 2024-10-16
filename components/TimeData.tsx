b6618e732cd99b13f144bf2e4b81d3cbdae03c4f
Uriel Campos <urielcampos448@gmail.com>
2024-10-16 16:49:23 -0300

    changed texts

--- components/TimeData.tsx
+++ components/TimeData.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react'
 import { Progress } from './ui/progress-bar'
+import { useLanguage } from '@/app/languageProvider'
 
 interface props {
   stage: string
@@ -10,6 +11,19 @@ interface props {
 }
 export const TimeData: React.FC<props> = ({ stage, progress, goal, value }): React.JSX.Element => {
   const [percentage, setPercentage] = useState(0)
+  const { language } = useLanguage()
+  const text: Record<number, Record<string, string>> = {
+    1: {
+      en: 'Secure your stake',
+      es: 'Asegure su participación',
+      pt: 'Garanta sua aposta'
+    },
+    2: {
+      en: 'Fractions Sold',
+      es: 'Fracciones vendidas',
+      pt: 'Frações vendidas'
+    }
+  }
   useEffect(() => {
     if (typeof progress === 'number' && typeof goal === 'number') {
       setPercentage((progress / goal) * 100)
@@ -19,10 +33,10 @@ export const TimeData: React.FC<props> = ({ stage, progress, goal, value }): Rea
     <div className='grid place-items-center gap-4 '>
         <div className='w-3/4 flex flex-col items-center font-bold'>
           <p className='text-sm uppercase md:text-base'>
-            Presale exclusive: 70% extra tokens
+            {text[1][language]}
           </p>
           <p className='text-sm uppercase md:text-base'>
-            token sold: {progress} / {goal} Unity
+          {text[2][language]} {progress} / {goal}
           </p>
         </div>
         <Progress value={percentage ?? 0} className='w-3/4 h-3 md:h-5 outline-dashed outline-1 outline-white'/>
