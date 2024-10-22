d5faadb1457cecc752586e398151b0e8068f9541
Uriel Campos <urielcampos448@gmail.com>
2024-10-22 17:51:59 -0300

    removed border from icons

--- components/IconSocial.tsx
+++ components/IconSocial.tsx
@@ -53,7 +53,7 @@ const networks = [
 const IconSocial = (): JSX.Element => {
   return (
         <div className='grid grid-cols-7 w-fit gap-6 rounded-full p-2 '>{networks.map((network) => (
-              <Link key={network.id} className='w-6 [&>svg]:stroke-primary-foreground [stroke-width:1px]'
+              <Link key={network.id} className='w-6'
                href={network.url} target='_blank'>
                 {network.image}
               </Link>
