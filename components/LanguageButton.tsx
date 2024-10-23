b0101214abc09d6c166f06538fb430272bcca91f
Uriel Campos <urielcampos448@gmail.com>
2024-10-22 23:11:59 -0300

    removed mexico flag

--- components/LanguageButton.tsx
+++ components/LanguageButton.tsx
@@ -10,7 +10,7 @@ const LanguageButton = (): React.JSX.Element => {
     <DropdownMenu>
         <DropdownMenuTrigger asChild>
         <ButtonUI variant="outline" size="icon" className='min-w-[2.5rem] bg-background/65 border-none'>
-            {language === 'es' && <Image src='/mxflag.png' alt='español' width={24} height={24}></Image>}
+            {language === 'es' && <Image src='/argentina.png' alt='español' width={24} height={24}></Image>}
             {language === 'en' && <Image src='/bandera.png' alt='ingles' width={24} height={24}></Image>}
             {language === 'pt' && <Image src='/brasil.png' alt='brasil' width={24} height={24}></Image>}
         </ButtonUI>
