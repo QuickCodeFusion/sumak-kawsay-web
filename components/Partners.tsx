a094c7f34d2db2267d58d998b736b07c86f100f2
Uriel Campos <urielcampos448@gmail.com>
2024-10-28 11:18:24 -0300

    adjusted Partners responsiveness

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -30,7 +30,7 @@ const Partners = (): JSX.Element => {
   return (
         <div className="my-8 space-y-4 uppercase text-center w-full h-64 md:scale-150">
             <h1>{text[1][language]}</h1>
-            <div className="flex justify-center gap-4 md:gap-24 w-full ">
+            <div className="flex flex-col md:flex-row justify-center gap-4 md:gap-24 w-full ">
                 {partners.map(partner => (
                     <div key={partner.name} className='flex items-center justify-center '>
                     <Image
