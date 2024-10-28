2697b57ed07b5a95d504ae4d3ff5130701a55db3
juanpo12 <juanjoc2000@hotmail.com>
2024-10-28 11:03:17 -0300

    added video to bg

--- components/BgVideo.tsx
+++ components/BgVideo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const BgVideo = (): JSX.Element => {
   return (
     <div className="absolute inset-0 h-[180vh] md:h-full w-full overflow-hidden bg-no-repeat bg-center md:pb-0 -z-50">
-        {/* <video
+        <video
             autoPlay
             loop
             muted
@@ -12,7 +12,7 @@ const BgVideo = (): JSX.Element => {
         >
             <source src="/Planetapaper.mp4" />
             Tu navegador no admite la reproducción de videos.
-        </video> */}
+        </video>
           <Image src="/bg6000.jpg" className='w-full object-cover h-full md:hidden' alt="bg" width={400} height={400} />
     </div>
   )
