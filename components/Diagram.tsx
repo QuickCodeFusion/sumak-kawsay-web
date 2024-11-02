1487711b1beb149e071fe4783c34958c04355792
Uriel Campos <urielcampos448@gmail.com>
2024-11-01 21:27:47 -0300

    fixed Diagram responsiveness

--- components/Diagram.tsx
+++ components/Diagram.tsx
@@ -25,7 +25,7 @@ export default function Diagram (): React.JSX.Element {
   return (
     <div className="w-full max-w-4xl mx-auto p-4 text-white text-center md:scale-125">
       <div className="relative flex flex-col md:space-y-40">
-        <div className="grid grid-cols-1 md:flex justify-around  gap-2">
+        <div className="grid grid-cols-1 md:flex justify-around gap-2">
           {elements.map((element, index) => (
             <Card key={index} className={`border ${element.borderColor} flex flex-col group justify-between items-center cursor-pointer contrast-125`}>
               <Image
@@ -34,10 +34,10 @@ export default function Diagram (): React.JSX.Element {
               width={128}
               height={128}
               className='group-hover:scale-105 group-hover:-translate-y-2 transition duration-300 drop-shadow-sm-white group-hover:drop-shadow-4xl-white'/>
-              <div className="text-center group-hover:scale-105 group-hover:-translate-y-2 transition duration-300 shadow-md rounded-sm p-2 space-y-2">
+              <div className="text-center group-hover:scale-105 group-hover:-translate-y-2 transition duration-300 shadow-md rounded-sm space-y-2 p-2">
                 <div className="font-bold">{element.name}</div>
                 <div className="text-sm">{text[language]}</div>
-                <div className='grid grid-cols-2'>
+                <div className='grid grid-cols-2 md:text-xs'>
                   <span>
                     <p>Return</p>
                     <p>APY {element.return}%</p>
