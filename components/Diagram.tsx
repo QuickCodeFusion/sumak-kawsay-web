02433770645e19e67e94cf284652c16ad90f9064
Uriel Campos <urielcampos448@gmail.com>
2024-11-01 21:14:40 -0300

    added Card and text to Diagram elements

--- components/Diagram.tsx
+++ components/Diagram.tsx
@@ -2,13 +2,14 @@ import { useLanguage } from '@/app/languageProvider'
 import { Card, CardContent } from '@/components/ui/card'
 import Image from 'next/image'
 import DashLine from './ui/llline'
+import { ButtonUI } from './ui/button'
 
 export default function Diagram (): React.JSX.Element {
   const elements = [
-    { name: 'EARTH', icon: '/level3.png', borderColor: 'border-[#00FF00] drop-shadow-[0_0_10px_#00FF00]' },
-    { name: 'WATER', icon: '/watericon.png', borderColor: 'border-[#0000FF] drop-shadow-[0_0_10px_#0000FF]' },
-    { name: 'AIR', icon: '/airicon.png', borderColor: 'border-[#FFFF00] drop-shadow-[0_0_10px_#FFFF00]' },
-    { name: 'FIRE', icon: '/level2.png', borderColor: 'border-[#FF0000] drop-shadow-[0_0_10px_#FF0000]' }
+    { name: 'EARTH', icon: '/level3.png', borderColor: 'border-[#00FF00] drop-shadow-[0_0_10px_#00FF00]', return: '12%', blocking: '12' },
+    { name: 'WATER', icon: '/watericon.png', borderColor: 'border-[#0000FF] drop-shadow-[0_0_10px_#0000FF]', return: '18%', blocking: '24' },
+    { name: 'AIR', icon: '/airicon.png', borderColor: 'border-[#FFFF00] drop-shadow-[0_0_10px_#FFFF00]', return: '24%', blocking: '36' },
+    { name: 'FIRE', icon: '/level2.png', borderColor: 'border-[#FF0000] drop-shadow-[0_0_10px_#FF0000]', return: '30%', blocking: '56' }
   ]
 
   const text: Record<string, string> = {
@@ -23,25 +24,38 @@ export default function Diagram (): React.JSX.Element {
 
   return (
     <div className="w-full max-w-4xl mx-auto p-4 text-white text-center md:scale-125">
-      <div className="relative flex flex-col md:space-y-16">
-        <div className="flex flex-wrap justify-around gap-2 mb-8">
+      <div className="relative flex flex-col md:space-y-40">
+        <div className="grid grid-cols-1 md:flex justify-around  gap-2">
           {elements.map((element, index) => (
-            <Card key={index} className={`border ${element.borderColor} flex flex-col group justify-between items-center space-y-2 p-4 cursor-pointer contrast-125`}>
+            <Card key={index} className={`border ${element.borderColor} flex flex-col group justify-between items-center cursor-pointer contrast-125`}>
               <Image
               src={element.icon}
               alt={element.name}
               width={128}
               height={128}
               className='group-hover:scale-105 group-hover:-translate-y-2 transition duration-300 drop-shadow-sm-white group-hover:drop-shadow-4xl-white'/>
-              <div className="text-center group-hover:scale-105 group-hover:-translate-y-2 transition duration-300 shadow-md">
+              <div className="text-center group-hover:scale-105 group-hover:-translate-y-2 transition duration-300 shadow-md rounded-sm p-2 space-y-2">
                 <div className="font-bold">{element.name}</div>
                 <div className="text-sm">{text[language]}</div>
+                <div className='grid grid-cols-2'>
+                  <span>
+                    <p>Return</p>
+                    <p>APY {element.return}%</p>
+                  </span>
+                  <span>
+                    <p>Blocking Time</p>
+                    <p>{element.blocking} Months</p>
+                  </span>
+                </div>
+                <ButtonUI>
+                  STAKE
+                </ButtonUI>
               </div>
             </Card>
           ))}
         </div>
 
-        <svg className="absolute top-40 left-0 w-full h-64 -z-50 hidden md:block" viewBox="0 0 400 200" preserveAspectRatio="none">
+        <svg className="absolute top-52 left-0 w-full h-64 -z-50 hidden md:block" viewBox="0 0 400 200" preserveAspectRatio="none">
           <path d="M50,0 V40" stroke="white" strokeWidth="1"/>
           <path d="M150,0 V40" stroke="white" strokeWidth="1"/>
           <path d="M250,0 V40" stroke="white" strokeWidth="1"/>
@@ -59,9 +73,9 @@ export default function Diagram (): React.JSX.Element {
           <path d="M0,200 V120" stroke="white" strokeWidth="1"/>
           <path d="M400,200 V120" stroke="white" strokeWidth="1"/>
         </svg>
-        <DashLine className="absolute z-50 hidden md:inline-block md:top-72 left-0 "></DashLine>
+        <DashLine className="absolute z-50 hidden md:inline-block md:top-96 left-0 "></DashLine>
 
-        <div className="grid grid-cols-2 md:flex justify-around gap-4 md:pt-8">
+        <div className="grid grid-cols-2 md:grid-cols-4 justify-around gap-4 ">
           {categories.map((category, index) => (
             <Card key={index} className="bg-blue-900 border-none cursor-pointer">
               <CardContent className="p-4">
