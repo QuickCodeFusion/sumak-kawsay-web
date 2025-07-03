8a0da85c90c3617bdd7bcdc4705e3080b49d693c
Uriel Campos <urielcampos448@gmail.com>
2025-07-03 08:00:43 -0300

    Update layout in BinanceWidget for improved image styling; adjust section alignment in page.tsx for better responsiveness.

--- components/BinanceWidget.tsx
+++ components/BinanceWidget.tsx
@@ -11,7 +11,7 @@ const BinanceWidget = (): JSX.Element => {
   }, [])
   return (
     <section className='flex flex-col gap-8 w-full'>
-      <Image className='p-0 -mt-48 scale-125 md:scale-110' src={'/fondofooter.png'} alt='fondo indigena' height={500} width={2000}></Image>
+      <Image className='pr-2 -mt-48 md:-mt-64 mb-36 md:mb-24 scale-[3.8] md:scale-110' src={'/fondofooter.png'} alt='fondo indigena' height={500} width={2000}></Image>
 
       <div className='binance-widget-marquee overflow-y-visible w-full'
         data-cmc-ids="1,1027,5426,1839,52,3408,5805,74,3890,7083"
